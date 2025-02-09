dcbc57e08e15751bdfb910ae04a703b374d41bd6
FurkanKovan <furkankovankaya@gmail.com>
2025-02-09 16:01:56 +0300

    cleanup on data access layer

--- src/app/folder/[folderId]/page.tsx
+++ src/app/folder/[folderId]/page.tsx
@@ -1,20 +1,20 @@
 import DriveContents from "../../drive-contents";
-import { getAllParentsForFolder, getFiles, getFolders } from "~/server/db/queries";
+import { DB_QUERIES } from "~/server/db/queries";
 
 export default async function GoogleDriveClone(props: {
   params: Promise<{ folderId: string }>;
 }) {
   const params = await props.params;
-  
+
   const parsedFolderId = parseInt(params.folderId);
   if (isNaN(parsedFolderId)) {
     return <div>Invalid folder ID!</div>;
   }
 
   const [folders, files, parents] = await Promise.all([
-    getFolders(parsedFolderId),
-    getFiles(parsedFolderId),
-    getAllParentsForFolder(parsedFolderId),
+    DB_QUERIES.getFolders(parsedFolderId),
+    DB_QUERIES.getFiles(parsedFolderId),
+    DB_QUERIES.getAllParentsForFolder(parsedFolderId),
   ]);
 
   return <DriveContents files={files} folders={folders} parents={parents} />;
