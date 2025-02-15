c8250c605a57ee192611edd1defe0c44a8f434e0
FurkanKovan <furkankovankaya@gmail.com>
2025-02-15 14:13:49 +0300

    change ut max file size

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -15,7 +15,7 @@ export const ourFileRouter = {
              * For full list of options and defaults, see the File Route API reference
              * @see https://docs.uploadthing.com/file-routes#route-config
              */
-            maxFileSize: "1GB",
+            maxFileSize: "32MB",
             maxFileCount: 10,
         },
     }).input(z.object({
